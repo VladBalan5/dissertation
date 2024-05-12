c07dec2e41e63000f88ae24555f1affadd17bdca
Vlad Balan <vlad.balan@edenred.com>
2024-05-12 13:48:37 +0300

    Register Screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/phone_number_verification_screen.dart';
 import 'package:chat_app/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -29,17 +30,65 @@ class LoginScreen extends StatelessWidget {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
-                await FirebaseAuth.instance.signInWithEmailAndPassword(
-                  email: _emailController.text.trim(),
-                  password: _passwordController.text.trim(),
-                );
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => ChatScreen(
-                      currentUserId: "U3FEwWy1JAWHzIpz8x1q",
-                    ),
-                  ),
-                );
+                try {
+                  // Sign in with email and password
+                  UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: _emailController.text.trim(),
+                    password: _passwordController.text.trim(),
+                  );
+
+                  // Check if the user has a phone number linked
+                  if (userCredential.user != null && (userCredential.user!.phoneNumber == null || userCredential.user!.phoneNumber!.isEmpty)) {
+                    // Redirect to phone number verification screen
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => PhoneNumberVerificationScreen(user: userCredential.user!)),
+                    );
+                  } else {
+                    // Redirect to home screen if phone number is already linked
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => ChatScreen(
+                        currentUserId: FirebaseAuth.instance.currentUser!.uid,
+                      )),
+                    );
+                  }
+                } catch (e) {
+                  print(e);  // Handle login error
+                  // Show error message
+                }
+
+                // try {
+                //   // Attempt to sign in.
+                //   await FirebaseAuth.instance.signInWithEmailAndPassword(
+                //     email: _emailController.text.trim(),
+                //     password: _passwordController.text.trim(),
+                //   );
+                //   Navigator.of(context).pushReplacement(
+                //     MaterialPageRoute(
+                //       builder: (context) => ChatScreen(
+                //         currentUserId: FirebaseAuth.instance.currentUser!.uid,
+                //       ),
+                //     ),
+                //   );
+                // } catch (e) {
+                //   print(e);
+                //   showDialog(
+                //     context: context,
+                //     builder: (ctx) => AlertDialog(
+                //       title: Text("Login Failed"),
+                //       content: Text(e.toString()),  // For a production app, you might want to display a more user-friendly message
+                //       actions: <Widget>[
+                //         TextButton(
+                //           onPressed: () {
+                //             Navigator.of(ctx).pop();  // Dismiss the dialog
+                //           },
+                //           child: Text('Okay'),
+                //         ),
+                //       ],
+                //     ),
+                //   );
+                // }
               },
               child: Text("Login"),
             ),
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,15 +1,19 @@
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 class RegisterScreen extends StatelessWidget {
   final _emailController = TextEditingController();
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _phoneNumberController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Register")),
+      appBar: AppBar(title: Text("Create account")),
       body: Padding(
         padding: EdgeInsets.all(20.0),
         child: Column(
@@ -28,18 +32,109 @@ class RegisterScreen extends StatelessWidget {
               decoration: InputDecoration(labelText: "Password"),
               obscureText: true,
             ),
+            TextField(
+              controller: _phoneNumberController,
+              decoration: InputDecoration(labelText: "Phone Number"),
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+              ],
+            ),
+            // TextFormField(
+            //   controller: _phoneNumberController,
+            //   decoration: InputDecoration(labelText: 'Phone Number'),
+            //   keyboardType: TextInputType.phone,
+            //   inputFormatters: [
+            //     FilteringTextInputFormatter.digitsOnly,  // Only allows digits to be entered
+            //     // You can add more formatters here to format the input as phone number
+            //   ],
+            //   validator: (value) {
+            //     if (value!.isEmpty || value.length != 10) {
+            //       return 'Enter a valid phone number';
+            //     }
+            //     return null;
+            //   },
+            // ),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
-                await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                  email: _emailController.text.trim(),
-                  password: _passwordController.text.trim(),
-                );
-                Navigator.pushReplacementNamed(context, '/chat');
+                registerUser(
+                    _emailController.text.trim(),
+                    _passwordController.text.trim(),
+                    _phoneNumberController.text,
+                    _usernameController.text,
+                    context);
               },
+              // onPressed: () async {
+              //   await FirebaseAuth.instance.createUserWithEmailAndPassword(
+              //     email: _emailController.text.trim(),
+              //     password: _passwordController.text.trim(),
+              //   );
+              //   Navigator.of(context).pushReplacement(
+              //     MaterialPageRoute(
+              //       builder: (context) => LoginScreen(),
+              //     ),
+              //   );
+              // },
               child: Text("Register"),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Future<void> registerUser(String email, String password, String phoneNumber,
+      String userName, BuildContext context) async {
+    FirebaseAuth _auth = FirebaseAuth.instance;
+    FirebaseFirestore _firestore = FirebaseFirestore.instance;
+    String defaultPicUrl = "https://static.vecteezy.com/system/resources/thumbnails/003/337/584/small/default-avatar-photo-placeholder-profile-icon-vector.jpg";
+
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Check if the user is really created and retrieved
+      User? user = userCredential.user;
+      if (user != null) {
+        await _firestore.collection('users').doc(user.uid).set({
+          'email': email,
+          'phoneNumber': phoneNumber,
+          'profilePicUrl': defaultPicUrl,
+          'userName': userName,
+          'userId': user.uid,
+          // Add other necessary fields
+        });
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => LoginScreen(),
+          ),
+        );
+      } else {
+        print("User object not returned.");
+      }
+    } catch (e) {
+      print("Error creating user: $e");
+      _showErrorDialog("Failed to register. Error: $e", context);
+    }
+  }
+
+  void _showErrorDialog(String message, BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Registration Failed'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Okay'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          ),
+        ],
       ),
     );
   }
